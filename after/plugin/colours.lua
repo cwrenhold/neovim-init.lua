c179f758f475f996befc00e16146d63da68a43eb
Catherine Wrenhold <40322939+cwrenhold@users.noreply.github.com>
2023-01-03 10:33:14 +0000

    Disabled italicised text from night-owl

--- after/plugin/colours.lua
+++ after/plugin/colours.lua
@@ -9,7 +9,24 @@ function ColorMyPencils(color)
 	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
+    -- Disable italicised text
+    toDisableItalicisation = {
+        "Comment",
+        "jsComment",
+        "jsImport",
+        "jsFrom",
+        "jsModuleAs",
+        "jsExtendsKeyword",
+        "javaScriptLineComment",
+        "cssClassName",
+        "cssPseudoClassId",
+        "cssIdentifier",
+        "shComment",
+        "NERDTreeHelp"
+    }
+    for k,v in pairs(toDisableItalicisation) do
+        vim.cmd(string.format([[highlight %s gui=NONE cterm=NONE]], v))
+    end
 end
 
 ColorMyPencils()
-
