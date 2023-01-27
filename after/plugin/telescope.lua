b0982ef40d65c417bd083b9645505185d6bb4da1
Catherine Wrenhold <40322939+cwrenhold@users.noreply.github.com>
2023-01-27 09:43:47 +0000

    Tweak to telescope grep, now <C-P>

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,6 +2,6 @@ local tsBuiltin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>pf', tsBuiltin.find_files, {})
 vim.keymap.set('n', '<C-p>', tsBuiltin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	tsBuiltin.grep_string({ search = vim.fn.input("Grep > ") })
+vim.keymap.set('n', '<C-P>', function()
+	tsBuiltin.grep_string({ search = vim.fn.input("Grep for text : ") })
 end)
